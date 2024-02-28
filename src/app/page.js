819d43887d0159745b23d4f2c5c140cde42bf863
na0260 <nur.201002398@green.ac.bd>
2024-02-28 19:26:40 +0600

    hero section added

--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,13 @@
 import { HelmetProvider } from 'react-helmet-async';
 import Navbar from "@/app/components/Navber";
 import Footer from "@/app/components/Footer";
+import HeroSection from "@/app/components/HeroSection";
 
 export default function Home() {
     return (
       <HelmetProvider>
           <Navbar/>
+          <HeroSection/>
           <Footer/>
       </HelmetProvider>
   );
