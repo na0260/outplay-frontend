c1ee7726684c5913c83dc80085a58e9fd57e13f9
na0260 <nur.201002398@green.ac.bd>
2024-03-02 13:59:53 +0600

    event component created

---
+++ src/app/components/Event.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+const Event = (props) => {
+    const logoStyle = {
+        background: 'transparent',
+        padding: '0',
+    };
+    return (
+        <div className="col-lg-4 col-md-12 gallery-item">
+            <div className="de-item wow">
+                <div className="d-overlay">
+                    <div className="d-text">
+                        <h4 className="pb-2">{props.title}</h4>
+                        <p className="d-price">Prizepool <span className="price">{props.prizepool}</span></p>
+                        <button className="btn-main" style={logoStyle}>
+                            <img src={props.logo}/>
+                        </button>
+                    </div>
+                </div>
+                <img src={props.img} className="img-fluid " alt={props.title}/>
+            </div>
+        </div>
+    );
+};
+
+export default Event;
