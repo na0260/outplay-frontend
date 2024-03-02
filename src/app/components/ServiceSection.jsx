95194c18346b40641013c321a4978f7f392c01c3
na0260 <nur.201002398@green.ac.bd>
2024-03-02 12:25:51 +0600

    Service Section added

---
+++ src/app/components/ServiceSection.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+
+const ServiceSection = () => {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-6">
+                    <div className="subtitle mb-3">WHAT DO WE DO?</div>
+                    <h2 className="mb20">Our Services</h2>
+                </div>
+
+                <div className="col-lg-6"></div>
+
+                <div className="col-lg-3 col-md-6 mb-sm-20">
+                    <div>
+                        <img src="./img/icons/1.png" className="mb20" alt=""/>
+                        <h4>GAMING & E-SPORTS EVENTS</h4>
+                        <p>Dolor minim in pariatur in deserunt laboris eu pariatur labore excepteur cupidatat cupidatat
+                            duis dolor in.</p>
+                    </div>
+                </div>
+
+                <div className="col-lg-3 col-md-6 mb-sm-20">
+                    <div>
+                        <img src="./img/icons/2.png" className="mb20" alt=""/>
+                        <h4>E-SPORTS BROADCASTING</h4>
+                        <p>Dolor minim in pariatur in deserunt laboris eu pariatur labore excepteur cupidatat cupidatat
+                            duis dolor in.</p>
+                    </div>
+                </div>
+
+                <div className="col-lg-3 col-md-6 mb-sm-20">
+                    <div>
+                        <img src="./img/icons/3.png" className="mb20" alt=""/>
+                        <h4>TALENT MANAGEMENT </h4>
+                        <p>Dolor minim in pariatur in deserunt laboris eu pariatur labore excepteur cupidatat cupidatat
+                            duis dolor in.</p>
+                    </div>
+                </div>
+
+                <div className="col-lg-3 col-md-6 mb-sm-20">
+                    <div>
+                        <img src="./img/icons/4.png" className="mb20" alt=""/>
+                        <h4>Sponsorships & Brand Integrations </h4>
+                        <p>Dolor minim in pariatur in deserunt laboris eu pariatur labore excepteur cupidatat cupidatat
+                            duis dolor in.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ServiceSection;
