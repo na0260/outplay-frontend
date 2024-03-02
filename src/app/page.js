e16e6345005d031e971750658cfcbb7d60d0c9ba
na0260 <nur.201002398@green.ac.bd>
2024-03-02 14:01:11 +0600

    events added

--- src/app/page.js
+++ src/app/page.js
@@ -4,20 +4,34 @@ import Navbar from "@/app/components/Navber";
 import Footer from "@/app/components/Footer";
 import HeroSection from "@/app/components/HeroSection";
 import ServiceSection from "@/app/components/ServiceSection";
+import {Parallax} from "react-parallax";
+import Events from "@/app/components/Events";
+
+const bg = "./img/events-bg.webp";
 
 export default function Home() {
     return (
         <HelmetProvider>
-            <header id="header-wrap">
-                <Navbar/>
-            </header>
-            <section className="no-padding">
-                <HeroSection/>
-            </section>
-            <section className="no-bottom tex-center">
-                <ServiceSection/>
-            </section>
+            <div className="home dark-scheme">
+                <header id="header-wrap">
+                    <Navbar/>
+                </header>
+                <section className="no-padding">
+                    <HeroSection/>
+                </section>
+                <section className="no-bottom tex-center">
+                    <ServiceSection/>
+                </section>
+                <Parallax className="" bgImage={bg} strength={300}>
+                    <div className="de-gradient-edge-top"></div>
+                    <div className="de-gradient-edge-bottom"></div>
+                    <section className="no-bg">
+                        <Events/>
+                    </section>
+                </Parallax>
                 <Footer/>
+            </div>
+
         </HelmetProvider>
-);
+    );
 }
