fc0a8e377e45508378fba21a32524741290cbfbe
na0260 <nur.201002398@green.ac.bd>
2024-03-02 12:40:26 +0600

    service section added

--- src/app/page.js
+++ src/app/page.js
@@ -3,13 +3,21 @@ import { HelmetProvider } from 'react-helmet-async';
 import Navbar from "@/app/components/Navber";
 import Footer from "@/app/components/Footer";
 import HeroSection from "@/app/components/HeroSection";
+import ServiceSection from "@/app/components/ServiceSection";
 
 export default function Home() {
     return (
-      <HelmetProvider>
-          <Navbar/>
-          <HeroSection/>
-          <Footer/>
-      </HelmetProvider>
-  );
+        <HelmetProvider>
+            <header id="header-wrap">
+                <Navbar/>
+            </header>
+            <section className="no-padding">
+                <HeroSection/>
+            </section>
+            <section className="no-bottom tex-center">
+                <ServiceSection/>
+            </section>
+                <Footer/>
+        </HelmetProvider>
+);
 }
