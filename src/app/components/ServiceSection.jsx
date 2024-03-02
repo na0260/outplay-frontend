300c65bcd24070797b75b48a044061f08468d0ba
na0260 <nur.201002398@green.ac.bd>
2024-03-02 12:40:41 +0600

    modified

--- src/app/components/ServiceSection.jsx
+++ src/app/components/ServiceSection.jsx
@@ -4,12 +4,10 @@ const ServiceSection = () => {
     return (
         <div className="container">
             <div className="row">
-                <div className="col-lg-6">
+                <div className="col-lg-12 text-center">
                     <div className="subtitle mb-3">WHAT DO WE DO?</div>
                     <h2 className="mb20">Our Services</h2>
                 </div>
-
-                <div className="col-lg-6"></div>
 
                 <div className="col-lg-3 col-md-6 mb-sm-20">
                     <div>
@@ -41,7 +39,7 @@ const ServiceSection = () => {
                 <div className="col-lg-3 col-md-6 mb-sm-20">
                     <div>
                         <img src="./img/icons/4.png" className="mb20" alt=""/>
-                        <h4>Sponsorships & Brand Integrations </h4>
+                        <h4 className="text-uppercase">Sponsorships & Brand Integrations </h4>
                         <p>Dolor minim in pariatur in deserunt laboris eu pariatur labore excepteur cupidatat cupidatat
                             duis dolor in.</p>
                     </div>
