07663b272f06fbe3d1fe48d6c3bab437406b041d
na0260 <nur.201002398@green.ac.bd>
2024-03-03 15:55:50 +0600

    brand logo section added

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,7 @@ import HeroSection from "@/app/components/HeroSection";
 import ServiceSection from "@/app/components/ServiceSection";
 import {Parallax} from "react-parallax";
 import Events from "@/app/components/Events";
+import BusinessPartners from "@/app/components/BusinessPartners";
 
 const bg = "./img/events-bg.webp";
 
@@ -16,12 +17,15 @@ export default function Home() {
                 <header id="header-wrap">
                     <Navbar/>
                 </header>
+
                 <section className="no-padding">
                     <HeroSection/>
                 </section>
+
                 <section className="no-bottom tex-center">
                     <ServiceSection/>
                 </section>
+
                 <Parallax className="" bgImage={bg} strength={300}>
                     <div className="de-gradient-edge-top"></div>
                     <div className="de-gradient-edge-bottom"></div>
@@ -29,6 +33,11 @@ export default function Home() {
                         <Events/>
                     </section>
                 </Parallax>
+
+                <section className="no-bottom">
+                    <BusinessPartners/>
+                </section>
+
                 <Footer/>
             </div>
 
