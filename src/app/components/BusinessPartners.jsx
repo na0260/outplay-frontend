356997173fd4541abe25ae717cf6adff37f2ed3a
na0260 <nur.201002398@green.ac.bd>
2024-03-03 15:55:18 +0600

    business partner section added

---
+++ src/app/components/BusinessPartners.jsx
@@ -0,0 +1,48 @@
+"use client";
+import React, {useCallback, useEffect, useState} from 'react';
+import BusinessPartnerLogo from "@/app/components/BusinessPartnerLogo";
+
+const BusinessPartners = () => {
+    return (
+        <>
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-12">
+                        <div className="subtitle fadeInUp mb-3">Our Valuable</div>
+                    </div>
+                    <div className="col-lg-12">
+                        <h2 className="fadeInUp mb20">Clients & Business Partners</h2>
+                    </div>
+                    <div className="spacer-10"></div>
+                </div>
+            </div>
+            <div className="no-bottom wow fadeInRight d-flex z-1000">
+                <div className="de-marquee-list wow">
+                    <div className="d-item">
+                        <BusinessPartnerLogo logo="./img/logos/mlbb.png"/>
+                        <BusinessPartnerLogo logo="./img/logos/mlbb.png"/>
+                        <BusinessPartnerLogo logo="./img/logos/mlbb.png"/>
+                        <BusinessPartnerLogo logo="./img/logos/mlbb.png"/>
+                        <BusinessPartnerLogo logo="./img/logos/mlbb.png"/>
+                        <BusinessPartnerLogo logo="./img/logos/mlbb.png"/>
+                        <BusinessPartnerLogo logo="./img/logos/mlbb.png"/>
+                        <BusinessPartnerLogo logo="./img/logos/mlbb.png"/>
+                    </div>
+                    <div className="d-item">
+                        <BusinessPartnerLogo logo="./img/logos/mlbb.png"/>
+                        <BusinessPartnerLogo logo="./img/logos/mlbb.png"/>
+                        <BusinessPartnerLogo logo="./img/logos/mlbb.png"/>
+                        <BusinessPartnerLogo logo="./img/logos/mlbb.png"/>
+                        <BusinessPartnerLogo logo="./img/logos/mlbb.png"/>
+                        <BusinessPartnerLogo logo="./img/logos/mlbb.png"/>
+                        <BusinessPartnerLogo logo="./img/logos/mlbb.png"/>
+                        <BusinessPartnerLogo logo="./img/logos/mlbb.png"/>
+                    </div>
+
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default BusinessPartners;
