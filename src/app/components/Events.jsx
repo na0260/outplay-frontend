da3d382eab8e44ddbf6cb63a3dd161ff7c2c2898
na0260 <nur.201002398@green.ac.bd>
2024-03-16 15:23:54 +0600

    modified

--- src/app/components/Events.jsx
+++ src/app/components/Events.jsx
@@ -4,25 +4,16 @@ import Event from "@/app/components/Event";
 
 const Events = () => {
     return (
-        <div className="container">
-            <div className="row">
-                <div className="col-md-6">
-                    <div className="subtitle mb20">Most recent works</div>
-                    <h2 className="wow fadeInUp">Portfolio</h2>
-                    <div className="spacer-20"></div>
-                </div>
-                <div className="col-lg-6 text-lg-end">
-                    <Link className="btn-main mb-sm-30" href="/events">View all events</Link>
-                </div>
-            </div>
             <div className="row g-4 sequence">
                 <Event title="MLBB SNOWSTORM SHOWDOWN" prizepool="$1200" img="./img/events/snowstorm.png" logo="./img/logos/mlbb.png"/>
                 <Event title="MLBB SNOWSTORM SHOWDOWN" prizepool="$1200" img="./img/events/snowstorm.png" logo="./img/logos/mlbb.png"/>
                 <Event title="MLBB SNOWSTORM SHOWDOWN" prizepool="$1200" img="./img/events/snowstorm.png" logo="./img/logos/mlbb.png"/>
                 <Event title="MLBB SNOWSTORM SHOWDOWN" prizepool="$1200" img="./img/events/snowstorm.png" logo="./img/logos/mlbb.png"/>
+                <Event title="MLBB SNOWSTORM SHOWDOWN" prizepool="$1200" img="./img/events/snowstorm.png" logo="./img/logos/mlbb.png"/>
+                <Event title="MLBB SNOWSTORM SHOWDOWN" prizepool="$1200" img="./img/events/snowstorm.png" logo="./img/logos/mlbb.png"/>
+                <Event title="MLBB SNOWSTORM SHOWDOWN" prizepool="$1200" img="./img/events/snowstorm.png" logo="./img/logos/mlbb.png"/>
+                <Event title="MLBB SNOWSTORM SHOWDOWN" prizepool="$1200" img="./img/events/snowstorm.png" logo="./img/logos/mlbb.png"/>
             </div>
-
-        </div>
     );
 };
 
